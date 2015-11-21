dcefa86a89131a54fbf054b66e4493d95a40ce05
jonalonsov <jonalonso@opendeusto.es>
2015-11-22 00:10:09 +0100

    login

---
+++ Proiektua/src/LP/LogIn.java
@@ -0,0 +1,30 @@
+package LP;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class LogIn extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField usuario;
+
+	public LogIn() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		usuario = new JTextField();
+		contentPane.add(usuario, BorderLayout.CENTER);
+		usuario.setColumns(10);
+	}
+
+}
--- Proiektua/src/LP/Main.java
+++ Proiektua/src/LP/Main.java
@@ -2,4 +2,11 @@ package LP;
 
 public class Main {
 
+	public static void main(String[] args) {
+
+		LogIn frame = new LogIn();
+		frame.setVisible(true);
+
+	}
+
 }
